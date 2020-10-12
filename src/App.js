697433f59423556f1b01e2f02dd591669f443819
sabinefrister <sabine@frister.net>
2020-10-12 20:44:27 +0200

    Add Headline and a little bit styling to the page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Container ><Button>Test</Button>
-        <h1>Hello World</h1>
-      </Container>
+			<Jumbotron> 
+			  <h1>Record audio and upload to Dropbox</h1>
+			  <p>
+			    You can record anything you like with this simple sound recorder. After naming your file, 
+			    you can upload it to Dropbox.
+			  </p>
+			</Jumbotron>
+			<Container>
+				<h2>Recorder</h2>
+			</Container>
     </div>
   );
 }
